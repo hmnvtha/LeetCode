97d17f92f445f3871ebcbed4bffff5c5a8c54529
hmnvtha <hmnvtha@gmail.com>
2025-01-08 23:22:32 +0530

    Time: 1622 ms (55.68%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT name from salesperson
+where sales_id not in 
+(
+    select sales_id from orders where com_id in 
+	(select com_id from company where name='RED')
+)
