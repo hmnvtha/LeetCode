549e4bfc0ce2f0a87dfb55248697273737896e94
hmnvtha <hmnvtha@gmail.com>
2025-01-10 23:53:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,8 @@
 class Solution {
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for(int i=2; i<=n; i++)
-        {
-            dp[i] = dp[i-1] + dp[i-2];
+        if (n == 0 || n == 1) {
+            return 1;
         }
-        return dp[n];
+        return climbStairs(n-1) + climbStairs(n-2);
     }
 }
