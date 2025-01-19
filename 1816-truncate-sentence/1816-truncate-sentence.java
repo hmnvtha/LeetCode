45c5ad74b09b38c373699537c7f46df6b8d7f1fd
hmnvtha <hmnvtha@gmail.com>
2025-01-19 22:44:06 +0530

    Time: 0 ms (100%), Space: 42.2 MB (46.11%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)==' '){
+               k--;
+            }
+            if(k==0){
+                return s.substring(0,i);
+            }
+        }
+        return s;
+    }
+}
