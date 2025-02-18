03e9bda3b9724a7beefdc6fcce62122b891fcf6a
hmnvtha <hmnvtha@gmail.com>
2025-02-18 16:26:13 +0530

    Time: 0 ms (100%), Space: 49.2 MB (53.06%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        int l = 0;
+        int r = n-1;
+        char temp = '0';
+
+        while(l<r)
+        {
+            temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+        
+    }
+}
